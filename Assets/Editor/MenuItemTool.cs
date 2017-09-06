191f3b6d15f235aa738299885ac4cd602693ac1f
NessChen <jaes54321@gmail.com>
2017-09-06 16:58:07 +0800

    QuickLoadScene

---
+++ Assets/Editor/MenuItemTool.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Elpis
+{
+    public class MenuItemTool : Editor
+    {
+        [MenuItem("Elpis/QuickLoadLoginScene %#l")]
+        static void QuickToLogin()
+        {
+            if (EditorSceneManager.GetActiveScene().isDirty)
+                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+        }
+
+        [MenuItem("Elpis/QuickLoadToolScene %#t")]
+        static void QuickToTool()
+        {
+            if (EditorSceneManager.GetActiveScene().isDirty)
+                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+            EditorSceneManager.OpenScene("Assets/Scenes/Tool.unity");
+        }
+    }
+}
+
